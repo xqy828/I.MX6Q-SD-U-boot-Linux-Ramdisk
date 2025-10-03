f0f4b939839755fbca351f695a88af1b6369610c
xqy <snown@live.cn>
2025-10-03 09:29:09 +0800

    upload mkimage tools for x86 platform

--- u-boot-2023.10-rc2/boot_scr/make_boot_scr.sh
+++ u-boot-2023.10-rc2/boot_scr/make_boot_scr.sh
@@ -3,5 +3,15 @@ CUR_DIR=$(pwd)
 CUR_DATE=$(date +%Y%m%d)
 CUR_TIME=$(date +%H%M%S)
 host_name=$(hostname)
-./mkimage -c none -A arm -T script -d ./boot.cmd.user ${CUR_DATE}_${CUR_TIME}_boot.scr
+ARCH=`uname -m`
+
+echo "ARCH=${ARCH}"
+if [ ${ARCH} = "x86_64" ]; then
+	./mkimage_x86 -c none -A arm -T script -d ./boot.cmd.user ${CUR_DATE}_${CUR_TIME}_boot.scr
+elif [ ${ARCH} = "aarch64" ]; then
+	./mkimage -c none -A arm -T script -d ./boot.cmd.user ${CUR_DATE}_${CUR_TIME}_boot.scr
+else
+	echo "arch is invalid"
+    exit 1
+fi
 cp ${CUR_DATE}_${CUR_TIME}_boot.scr boot.scr
